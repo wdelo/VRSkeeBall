ae8bfff2d869dd2998d83be1fe4b97a123c47a22
wdelo <55678535+wdelo@users.noreply.github.com>
2020-02-04 10:34:11 -0500

    Added VRBase, configured Visual Studio and Unreal, added teleportation bounds - Will

--- Source/DemoSkeeBallProject/DemoSkeeBallProject.Build.cs
+++ Source/DemoSkeeBallProject/DemoSkeeBallProject.Build.cs
@@ -1,23 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+using System.IO;
 using UnrealBuildTool;
 
-public class DemoSkeeBallProject : ModuleRules
-{
-	public DemoSkeeBallProject(ReadOnlyTargetRules Target) : base(Target)
-	{
+public class DemoSkeeBallProject : ModuleRules {
+	public DemoSkeeBallProject(ReadOnlyTargetRules Target) : base(Target) {
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Slate", "SlateCore", "ProceduralMeshComponent" });
+        PrivateDependencyModuleNames.AddRange(new string[] { "HeadMountedDisplay", "SteamVR" });
+        
+        PublicIncludePaths.Add(Path.GetFullPath(Path.Combine(ModuleDirectory, "VRBase")));
+    }
 }
